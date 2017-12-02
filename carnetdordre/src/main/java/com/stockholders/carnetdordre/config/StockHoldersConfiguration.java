f208523771c80ac27e5fdaf551511d0433c0f6f4
MarcBojoly <mbojoly@octo.com>
2017-12-02 15:19:09 +0100

    Make Gateway URL configurable

--- carnetdordre/src/main/java/com/stockholders/carnetdordre/config/StockHoldersConfiguration.java
+++ carnetdordre/src/main/java/com/stockholders/carnetdordre/config/StockHoldersConfiguration.java
@@ -1,4 +1,20 @@
 package com.stockholders.carnetdordre.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "stockholders")
 public class StockHoldersConfiguration {
+
+    private String apiGatewayUrl;
+
+    public String getApiGatewayUrl() {
+        return apiGatewayUrl;
+    }
+
+    public void setApiGatewayUrl(String apiGatewayUrl) {
+        this.apiGatewayUrl = apiGatewayUrl;
+    }
 }
