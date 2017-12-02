4cf7dbd73fc257c00270a6fce8afa234766e2cf9
bouluad <med.bouluad@gmail.com>
2017-12-02 04:19:49 +0100

    object product

---
+++ catalogue/src/main/java/com/stockholders/catalogue/domain/Product.java
@@ -0,0 +1,131 @@
+package com.stockholders.catalogue.domain;
+
+import io.swagger.annotations.ApiModel;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import javax.validation.constraints.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+
+/**
+ * not an ignored comment
+ */
+@ApiModel(description = "not an ignored comment")
+@Document(collection = "product")
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private String id;
+
+    @NotNull
+    @Field("name")
+    private String name;
+
+    @Field("slug")
+    private String slug;
+
+    @NotNull
+    @Field("price")
+    private Long price;
+
+    @NotNull
+    @Field("number")
+    private Long number;
+
+    // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Product name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public Product slug(String slug) {
+        this.slug = slug;
+        return this;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public Product price(Long price) {
+        this.price = price;
+        return this;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public Long getNumber() {
+        return number;
+    }
+
+    public Product number(Long number) {
+        this.number = number;
+        return this;
+    }
+
+    public void setNumber(Long number) {
+        this.number = number;
+    }
+    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here, do not remove
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        if (product.getId() == null || getId() == null) {
+            return false;
+        }
+        return Objects.equals(getId(), product.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+            "id=" + getId() +
+            ", name='" + getName() + "'" +
+            ", slug='" + getSlug() + "'" +
+            ", price=" + getPrice() +
+            ", number=" + getNumber() +
+            "}";
+    }
+}
